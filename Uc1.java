5c37c906930aa595e202423fd402d82e02d50de0
immanuveljeeva08 <immanuveljeeva2000@gmail.com>
2021-07-11 12:47:55 -0700

    Use Case1

---
+++ Uc1.java
@@ -0,0 +1,22 @@
+/*****************************************************************************************************************
+ *
+ *`Rule1 -> Snake and Ladder game played at player position 0
+ *
+ * @author Immanuvel Jeeva
+ * @Since 11-07-2021
+ *
+ *****************************************************************************************************************/
+
+public class Uc1 {
+
+	public static void main(String[] args) {
+
+		//Initially we start the game with one player
+		int player=1;
+
+		// The player in position 0
+		int position=0;
+
+		System.out.println("Player in the position = "+position);
+	}
+}
